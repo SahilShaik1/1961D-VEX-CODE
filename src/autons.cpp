2a62b09a6f96a4c4ce1168e0cbdafb3bd8202149
Sahil Shaik <89698739+SahilShaik1@users.noreply.github.com>
2024-02-18 16:33:18 -0500

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -2,16 +2,10 @@
 #include "initialize.cpp"
 #define REVERSE true
 
-/////
-// For instalattion, upgrading, documentations and tutorials, check out website!
-// https://ez-robotics.github.io/EZ-Template/
-/////
 
 
-const int DRIVE_SPEED = 110; // This is 110/127 (around 87% of max speed).  We don't suggest making this 127.
-                             // If this is 127 and the robot tries to heading correct, it's only correcting by
-                             // making one side slower.  When this is 87%, it's correcting by making one side
-                             // faster and one side slower, giving better heading correction.
+
+const int DRIVE_SPEED = 110; 
 const int TURN_SPEED  = 90;
 const int SWING_SPEED = 90;
 const int TILE_FACTOR = 24;
